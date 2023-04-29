3703ecb7f17271fb6419b5c7d065b6bee2e31514
SnesFX <108774265+TyDevX@users.noreply.github.com>
2023-04-29 19:07:07 -0400

    erm main release

---
+++ Assets/Crappy Code/RadiantLauncher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RadiantLauncher : MonoBehaviour
+{
+ 
+    public void LaunchGenshin()
+    {
+        Process.Start("C:/Program Files/Genshin Impact/Genshin Impact Game/GenshinImpact.exe");
+    }
+    
+    public void LaunchHonkai()
+    {
+        Process.Start("C:/Program Files/Honkai Impact 3rd glb/Games/BH3.exe");
+    }
+
+    public void LaunchStarRail()
+    {
+        Process.Start("C:/Program Files/Star Rail/Games/StarRail.exe");
+    }
+
+    public void GenshinScene()
+    {
+        SceneManager.LoadScene("Genshin");
+    }
+
+    public void HonkaiScene()
+    {
+        SceneManager.LoadScene("Honkai");
+    }
+
+    public void StarRailScene()
+    {
+        SceneManager.LoadScene("StarRail");
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("RadiantLauncher");
+    }
+}
---
+++ Changelog.md
@@ -0,0 +1,7 @@
+# 1.0 Release
+
+* Woo silly main release.
+
+* Supports: Genshin Impact, Honkai Impact 3rd, and Honkai: Star Rail
+
+* Send suggestions to me on discord! TyDevMD#9787
